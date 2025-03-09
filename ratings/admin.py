390dd1e4cb32d15f0992dfc27779aa521c23dc79
sc22thnl <123957256+Azorahai237@users.noreply.github.com>
2025-03-09 01:08:53 +0000

    update admin

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Professor, Module, ModuleInstance, Rating
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('module_instance', 'professor', 'user', 'rating', 'last_updated')
+    search_fields = ('professor__name', 'module_instance__module__name', 'user__username')
+
 
 admin.site.register(Professor)
 admin.site.register(Module)
 admin.site.register(ModuleInstance)
-admin.site.register(Rating)
+admin.site.register(Rating, RatingAdmin)
